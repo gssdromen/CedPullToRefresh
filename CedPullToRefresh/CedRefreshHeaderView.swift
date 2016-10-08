ed03f88a40a7c5a174669d851d67ee7a43b20bea
gssdromen <gssdromen@gssdromens-Mac-mini.local>
2016-10-09 07:14:26 +0800

    some init

--- CedPullToRefresh/CedRefreshHeaderView.swift
+++ CedPullToRefresh/CedRefreshHeaderView.swift
@@ -9,7 +9,41 @@
 import UIKit
 
 class CedRefreshHeaderView: UIView, CedLoadingProtocol {
-
     
+    var needsLayout = true
 
+    // MARK: - CedLoadingProtocol
+    func startPulling() {
+        
+    }
+    
+    func releaseToRefresh() {
+        
+    }
+    
+    func refreshing() {
+        
+    }
+    
+    func finishRefresh() {
+        
+    }
+    
+    // MARK:
+    
+    // MARK: - Life Cycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        if self.needsLayout {
+            
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
