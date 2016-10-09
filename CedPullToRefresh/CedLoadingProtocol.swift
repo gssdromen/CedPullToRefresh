3c4c049a153bd6f69d0dff399304e5ddb0646bcd
wuyinjun <wuyinjun@fangdd.com>
2016-10-09 18:41:47 +0800

    add files

--- CedPullToRefresh/CedLoadingProtocol.swift
+++ CedPullToRefresh/CedLoadingProtocol.swift
@@ -6,9 +6,20 @@
 //  Copyright © 2016年 CedricWu. All rights reserved.
 //
 
-@objc public protocol CedLoadingProtocol {
-    @objc optional func startPulling()
-    @objc optional func releaseToRefresh()
-    @objc optional func refreshing()
-    @objc optional func finishRefresh()
+public enum LoadingState {
+    case Pulling
+    case ReleaseToRefresh
+    case Refreshing
+    case Finished
+}
+
+public protocol CedLoadingProtocol {
+    func startPulling()
+    func releaseToRefresh()
+    func refreshing()
+    func finishRefresh()
+
+    var loadingState: LoadingState { get set }
+    var triggeredByUser: Bool { get set }
+    var actionHandler: (() -> Void)? { get set }
 }
--- CedPullToRefresh/CedRefreshHeaderView.swift
+++ CedPullToRefresh/CedRefreshHeaderView.swift
@@ -10,40 +10,89 @@ import UIKit
 
 class CedRefreshHeaderView: UIView, CedLoadingProtocol {
     
-    var needsLayout = true
+    internal var loadingState: LoadingState = LoadingState.Finished {
+        willSet {
+            if self.loadingState != newValue {
+                // 更新UI
+                switch newValue{
+                case LoadingState.Finished:
+                    self.finishRefresh()
+                    break
+                case LoadingState.Pulling:
+                    self.startPulling()
+                    break
+                case LoadingState.Refreshing:
+                    self.refreshing()
+                    break
+                case LoadingState.ReleaseToRefresh:
+                    self.releaseToRefresh()
+                    break
+                }
+            }
+        }
+        didSet {
 
-    // MARK: - CedLoadingProtocol
-    func startPulling() {
-        
-    }
-    
-    func releaseToRefresh() {
-        
-    }
-    
-    func refreshing() {
-        
-    }
-    
-    func finishRefresh() {
-        
-    }
-    
-    // MARK:
-    
-    // MARK: - Life Cycle
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        if self.needsLayout {
-            
         }
     }
     
+    internal var triggeredByUser: Bool = false
+    
+    internal var actionHandler: (() -> Void)? = nil
+
+    var needsLayout = true
+    let activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.gray)
+
+    // MARK: - CedLoadingProtocol
+    func startPulling(offset: CGPoint) {
+        
+    }
+
+    func releaseToRefresh(offset: CGPoint) {
+
+    }
+
+    func refreshing(offset: CGPoint) {
+
+    }
+
+    func finishRefresh(offset: CGPoint) {
+
+    }
+    
+    // MARK: - Public Methods
+    public func getLoadingState() -> LoadingState {
+        return self.loadingState
+    }
+    
+    public func isTriggeredByUser() -> Bool {
+        return self.triggeredByUser
+    }
+
+    // MARK: - Views About
+    func addMyViews() {
+        self.activityIndicator.hidesWhenStopped = true
+        self.addSubview(self.activityIndicator)
+    }
+
+    func layoutMyViews() {
+        self.activityIndicator.center = CGPoint(x: self.bounds.width / 2, y: self.bounds.height / 2)
+    }
+
+    // MARK: - Life Cycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.addMyViews()
+    }
+    
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        if self.needsLayout {
+            self.layoutMyViews()
+            self.needsLayout = false
+        }
     }
 }
